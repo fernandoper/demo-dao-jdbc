2d1e2198450e5a48c57d44aac89cbe708ace31ac
Fernando <fernandoreisper@gmail.com>
2024-01-14 19:40:12 -0300

    seller entity

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,17 +3,22 @@ package application;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Date;
 
 import db.DB;
 import db.DbIntegrityException;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
     public static void main(String[] args) {
 
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+
+        Seller seller = new Seller(21, "Bob","bob@gmail.com", new Date(), 3000.00, obj);
+
+        System.out.println(seller);
 
     }
 }
